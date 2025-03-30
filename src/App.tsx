e9ee66631469c3318ec83dd0a3c39508fd4d5e8b
naoya5 <naoya2115@outlook.jp>
2025-03-30 20:25:55 +0900

    ログインしていたらリダイレクトされないように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import { Home } from "./pages/Home";
 import NoteDetail from "./pages/NoteDetail";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
+import { useCurrentUserStore } from "./modules/auth/current-user.state";
+import { authRepository } from "./modules/auth/auth.repository";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const currentUserStore = useCurrentUserStore();
+
+  //マウント時にsupabaseのsessionを取得して、currentUserStoreにセットする
+  useEffect(() => {
+    setSession();
+  }, []);
+
+  // supabaseのsessionを取得して、currentUserStoreにセットする
+  const setSession = async () => {
+    const currentUser = await authRepository.getCurrentUser();
+    currentUserStore.set(currentUser);
+    setIsLoading(false);
+  };
+
+  // ローディング中は何も表示しない
+  //setSessionが終わって、ログイン済みかどうか判別してから、画面に飛ばす
+  if (isLoading) return <div />;
+
   return (
     //Routing
     <BrowserRouter>
       <div className="h-full">
         <Routes>
           <Route path="/" element={<Layout />}>
+            {/* loginしている場合のみ表示、それ以外ならログインページに飛ばす */}
             <Route index element={<Home />} />
             <Route path="/notes/:id" element={<NoteDetail />} />
           </Route>
