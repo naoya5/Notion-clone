f42e67a3229ba596bcc72cf9f268b1dbf507c555
naoya5 <naoya2115@outlook.jp>
2025-04-02 14:02:45 +0900

    supabaseの処理追加

--- src/modules/notes/note.repository.ts
+++ src/modules/notes/note.repository.ts
@@ -9,12 +9,25 @@ export const noteRepository = {
           //notesテーブルにレコードを追加
           user_id: userId,
           title: params.title,
-          parent_id: params.parentId,
+          parent_document: params.parentId,
         },
       ])
       .select() //配列で取得
       .single(); //単一のレコードを取得
     if (error != null) throw new Error(error.message);
     return data; //notesテーブルのレコードを返す
+  },
+  //ノートを取得
+  async find(userId: string, parentDocumentId: number) {
+    const query = supabase
+      .from("notes")
+      .select()
+      .eq("user_id", userId)
+      .order("created_at", { ascending: false });
+    const { data } =
+      parentDocumentId != null
+        ? await query.eq("parent_document", parentDocumentId)
+        : await query.is("parent_document", null);
+    return data;
   },
 };
