454cb5d72f14906ffee824a0c94463c1b4aef2b9
naoya5 <naoya2115@outlook.jp>
2025-04-02 14:03:35 +0900

    グローバルステートに追加処理を作成

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,8 +1,12 @@
-import { Outlet } from 'react-router-dom';
-import SideBar from './components/SideBar';
-import { SearchModal } from './components/SearchModal';
+import { Navigate, Outlet } from "react-router-dom";
+import SideBar from "./components/SideBar";
+import { SearchModal } from "./components/SearchModal";
+import { useCurrentUserStore } from "./modules/auth/current-user.state";
 
 const Layout = () => {
+  const { currentUser } = useCurrentUserStore();
+  //ユーザーがnullの場合はログインページに飛ばす
+  if (currentUser == null) return <Navigate replace to="/signin" />;
   return (
     <div className="h-full flex">
       <SideBar onSearchButtonClicked={() => {}} />
